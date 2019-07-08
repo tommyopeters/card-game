8bff61ad8bca07579e132cd419320f889da2981f
Tommy O' Peters <tommyopeters>
2019-07-08 14:28:55 +0100

    Fixed Stash Errors

--- js/GameEngine.js
+++ js/GameEngine.js
@@ -24,12 +24,14 @@ DealerDeck.shuffle();
 let Game = new GameEngine;
 // Game.start();
 
-
+//REMOVE .STARTGAME TO START GAME
 fadeOut('.startgame', 1, '.yes');
+
+//SHAKE .STARTGAME TO REFUSE NO OPTION
 shakeIt('.startgame', 1, '.no');
 
 
-
+//MAKE CHIPS TABLE VISIBLE AFTER MAIN GAME TAB HAS BEEN LOADED
 document.querySelector('.yes').addEventListener('click', (e)=>{
     setTimeout(() => {
         document.querySelector('.people').style.removeProperty('display');
@@ -42,11 +44,59 @@ document.querySelector('.yes').addEventListener('click', (e)=>{
     }, 500);
 })
 
-document.querySelector('.prebet').addEventListener('click', ()=>{
-    document.querySelector('.prebet').style.display= "none";
-    document.querySelector('.postbet').classList.remove('hidden');
-    document.querySelector('.chipstable').style.display = "none";
+//RESET AMOUNT BET
+document.querySelector('.reset').addEventListener('click', ()=>{
+    document.querySelector('#amount').innerHTML = "--";
+    amountbet = 0;
 })
 
 
-document.querySelector('.yes').click();
+//INCREMENT AMOUNT BET VALUE BY POKERCHIP SELECTED
+
+for(let i=0; i<pokerchips.length; i++){
+    pokerchips[i].addEventListener('click', ()=>{
+        
+        if(pokerchips[i].classList.contains('white')){
+            amountbet+=1;
+            if(amountbet>500){amountbet=500};
+            document.getElementById('amount').innerHTML = amountbet;
+        }
+        if(pokerchips[i].classList.contains('red')){
+            amountbet+=5;
+            if(amountbet>500){amountbet=500};
+            document.getElementById('amount').innerHTML = amountbet;
+        }
+        if(pokerchips[i].classList.contains('blue')){
+            amountbet+=10;
+            if(amountbet>500){amountbet=500};
+            document.getElementById('amount').innerHTML = amountbet;
+        }
+        if(pokerchips[i].classList.contains('green')){
+            amountbet+=25;
+            if(amountbet>500){amountbet=500};
+            document.getElementById('amount').innerHTML = amountbet;
+        }
+        if(pokerchips[i].classList.contains('black')){
+            amountbet+=100;
+            if(amountbet>500){amountbet=500};
+            document.getElementById('amount').innerHTML = amountbet;
+        }
+
+    })
+}
+
+//REMOVE CHIPS TABLE AND REPLACE PREBET WITH POSTBET WHEN BET HAS BEEN MADE
+document.querySelector('.bet').addEventListener('click', ()=>{
+    if(amountbet==0){
+        document.querySelector('.bet').classList.add('apply-shake');
+        document.querySelector('.bet').addEventListener("animationend", (e) => {
+            document.querySelector('.bet').classList.remove("apply-shake");
+            return;
+        })
+    }else{
+        document.querySelector('.prebet').style.display= "none";
+        document.querySelector('.postbet').classList.remove('hidden');
+        document.querySelector('.chipstable').style.display = "none";
+    }
+})
+
